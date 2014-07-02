f74c6a02c61de42513d5317bc0653d99d4b7e380
YuuShigetani <s2g4t1n2@gmail.com>
2014-07-03 03:27:50 +0900

    openmpi package install

--- mpi/chef-repo/site-cookbooks/openmpi/attributes/default.rb
+++ mpi/chef-repo/site-cookbooks/openmpi/attributes/default.rb
@@ -1,5 +1,16 @@
+default['openmpi']['package']           = false
+default['openmpi']['package']['debian'] = %w(openmpi-bin libopenmpi-dev)
+
 default['openmpi']['version']   = '1.8.1'
 default['openmpi']['url']       = "http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-#{node['openmpi']['version']}.tar.gz"
-default['openmpi']['src_dir']   = '/usr/local/src'
 default['openmpi']['prefix']    = '/usr/local'
 default['openmpi']['make_opts'] = %w{ -j2 }
+default['openmpi']['packages']  = %w{
+  autotools-dev
+  autoconf
+  automake
+  g++
+  gfortran
+  build-essential
+  flex
+}
--- mpi/chef-repo/site-cookbooks/openmpi/recipes/default.rb
+++ mpi/chef-repo/site-cookbooks/openmpi/recipes/default.rb
@@ -7,23 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{
-  autotools-dev
-  autoconf
-  automake
-  g++
-  gfortran
-  build-essential
-  flex
-}.each do |pkg|
-  package pkg
-end
+if node['openmpi']['package']
+  packages = node['package'][node['platform_family']] || []
+  packages.each { |pkg| package pkg }
+else
+  node['openmpi']['packages'].each do |pkg|
+    package pkg
+  end
 
-ark 'openmpi' do
-  owner     node['user']
-  url       node['openmpi']['url']
-  version   node['openmpi']['version']
-  make_opts node['openmpi']['make_opts']
-  timeout   36000
-  action    :install_with_make
+  ark 'openmpi' do
+    owner     node['user']
+    url       node['openmpi']['url']
+    version   node['openmpi']['version']
+    make_opts node['openmpi']['make_opts']
+    timeout   36000
+    action    :install_with_make
+  end
 end
